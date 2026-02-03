e9e126ad8d6d86cd8085346ac7f8d4b58a63bbbc
hactuss <hactuss@proton.me>
2026-02-03 21:26:01 +0100

    deleted:    .prettierignore 	deleted:    .prettierrc 	modified:   src/app.css 	modified:   src/lib/components/ProjectCard.svelte 	modified:   src/routes/+page.svelte

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,29 +1,40 @@
 <script>
-    export let title;
-    export let discription;
-    export let url;
-    export let buildtime;
-    export let url_gh;
+  import { flip } from "svelte/animate";
+  import { fade } from "svelte/transition";
+
+  export let title;
+  export let discription;
+  export let url;
+  export let buildtime;
+  export let url_gh;
 </script>
 
 <main>
-    <h2>{title}</h2>
-    <p>started {buildtime}</p>
-    <p>{discription}</p>
-    <div>
-        <a href={url}>Visit ⇱</a>
-        <a href={url_gh}>Source ⇱</a>
-    </div>
+  <h2>{title}</h2>
+  <p>started {buildtime}</p>
+  <p>{discription}</p>
+  <div>
+    <a href={url}>Visit ⇱</a>
+    <a href={url_gh}>Source ⇱</a>
+  </div>
 </main>
 
 <style>
-    main {
-        background-color: transparent;
-        color: white;
-        border: white solid 1px;
-        border-radius: 10px;
-        padding: 5px;
-        margin-bottom: 3%;
-        backdrop-filter: blur(5px);
-    }
+  main {
+    background-color: transparent;
+    color: white;
+    border: white solid 1px;
+    border-radius: 10px;
+    padding: 5px;
+    margin-bottom: 3%;
+    backdrop-filter: blur(5px);
+
+    background: #141414;
+    background: linear-gradient(
+      0deg,
+      rgba(20, 20, 20, 1) 50%,
+      rgba(34, 34, 34, 1) 74%,
+      rgba(50, 50, 50, 1) 90%
+    );
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import ProjectCard from '$lib/components/ProjectCard.svelte';
-	import ghlogo from '$lib/assets/images/github-mark-white.svg';
+  import ProjectCard from "$lib/components/ProjectCard.svelte";
+  import ghlogo from "$lib/assets/images/github-mark-white.svg";
 </script>
 
 <!--
@@ -16,136 +16,154 @@
 </pre> -->
 
 <main id="MAIN_CONTAINER">
-	<main id="SIDEBAR">
-		<span id="SIDEBAR_TEXT">H A C T U S S</span>
-		<div id="gh_link">
-			<a href="https://github.com/hactuss" id="githublogo">
-				<img src={ghlogo} alt="github_logo" />
-			</a>
-		</div>
-	</main>
-	<main id="MAIN_CONTENTS">
-		<header>
-			<h1>Name: <span id="marked1">Hactuss</span></h1>
-			<ul>
-				<li>Frontend webdevelopment as a hobby</li>
-				<li>
-					Primary programming language: <a id="marked2" href="https://svelte.dev/">Svelte</a>
-					and
-					<a id="marked3" href="https://sass-lang.com/">SCSS</a>
-				</li>
+  <main id="SIDEBAR">
+    <span id="SIDEBAR_TEXT">H A C T U S S</span>
+    <div id="gh_link">
+      <a href="https://github.com/hactuss" id="githublogo">
+        <img src={ghlogo} alt="github_logo" />
+      </a>
+    </div>
+  </main>
+  <main id="MAIN_CONTENTS">
+    <header>
+      <div class="header-1">
+        <h1>Name: <span id="marked1">Hactuss</span></h1>
+        <ul>
+          <li>Frontend webdevelopment as a hobby</li>
+          <li>
+            Primary programming language: <a
+              id="marked2"
+              href="https://svelte.dev/">Svelte</a
+            >
+            and
+            <a id="marked3" href="https://sass-lang.com/">SCSS</a>
+          </li>
 
-				<li>
-					Currently learning <a id="marked5" href="https://rust-lang.org"> Rust </a>
-				</li>
+          <li>
+            Currently learning <a id="marked5" href="https://rust-lang.org">
+              Rust
+            </a>
+          </li>
 
-				<li>Seeking job in IT industry</li>
-			</ul>
-		</header>
-		<hr />
-		<div id="Projects_wrapper">
-			<h2>Projects</h2>
-			<ProjectCard
-				title="Portfolio"
-				buildtime="Nov 2. 2025"
-				discription="This portfolio I created to tell about my projects and work."
-				url="/"
-				url_gh="https://github.com/hactuss/portfolio"
-			/>
-			<ProjectCard
-				title="Center Calculator"
-				buildtime="Sep 27. 2025"
-				discription="A web app to automate the process of calculating the center of a given area. Useful in graphic design and more."
-				url="https://center-calc.vercel.app"
-				url_gh="https://github.com/hactuss/ccenter"
-			/>
-			<ProjectCard
-				title="Worldclock"
-				buildtime="Dec 31. 2023"
-				discription="a website to show the current time in different timezones. Useful when interacting with people from all around the world. (Currently being rewritten in svelte.)"
-				url="https://hactuss.github.io/pages/office/office.html"
-				url_gh="https://github.com/hactuss/hactuss.github.io"
-			/>
-			<ProjectCard
-				title="It is spooky month?"
-				buildtime="Sep 15. 2025"
-				discription="a small jokewebsite I built in reference to the Spooky Month indie animation series. Shows something special in october."
-				url="https://isitspookymonth.vercel.app"
-				url_gh="https://github.com/hactuss/isitspookymonthdotcom"
-			/>
-			<ProjectCard
-				title="Ultrakill Terminal"
-				buildtime="Nov 29. 2025"
-				discription="a recreation of the terminals from the indiegame 'Ultrakill'"
-				url="https://uk-terminal.vercel.app"
-				url_gh="https://github.com/hactuss/uk-terminal"
-			/>
-			<ProjectCard
-				title="Aero Calendar"
-				buildtime="Dec 9. 2025"
-				discription="A simple yet funcional calendar in the Aero style"
-				url="https://aerocalendar.vercel.app"
-				url_gh="https://github.com/hactuss/aerocalendar"
-			/>
-		</div>
-		<hr />
-		<div>
-			<h2>Personality</h2>
-			<div>
-				<h3>Hobbies</h3>
-				<ul>
-					<li>Videoediting</li>
-					<li>
-						Experimenting with Linux (Specifically <a id="marked4" href="https://nixos.org/"
-							>NixOS</a
-						>)
-					</li>
-					<li>Drawing comics</li>
-					<li>Reading books regarding my hobbies or general literature</li>
-					<li>Gaming</li>
-				</ul>
-			</div>
-		</div>
-		<hr />
-		<footer>
-			<p>
-				Contact me via Email: <a href="mailto:hactuss@proton.me"><span id="marked1">hactuss@proton.me</span></a>
-			</p>
-		</footer>
-	</main>
+          <li>Seeking job in IT industry</li>
+        </ul>
+      </div>
+      <!-- --
+      <div class="header-2" style:border="1px solid white">
+        <h2>second box</h2>
+        </div>-->
+    </header>
+    <hr />
+    <div id="Projects_wrapper">
+      <h2>Projects</h2>
+      <ProjectCard
+        title="Portfolio"
+        buildtime="Nov 2. 2025"
+        discription="This portfolio I created to tell about my projects and work."
+        url="/"
+        url_gh="https://github.com/hactuss/portfolio"
+      />
+      <ProjectCard
+        title="Center Calculator"
+        buildtime="Sep 27. 2025"
+        discription="A web app to automate the process of calculating the center of a given area. Useful in graphic design and more."
+        url="https://center-calc.vercel.app"
+        url_gh="https://github.com/hactuss/ccenter"
+      />
+      <ProjectCard
+        title="Worldclock"
+        buildtime="Dec 31. 2023"
+        discription="a website to show the current time in different timezones. Useful when interacting with people from all around the world. (Currently being rewritten in svelte.)"
+        url="https://hactuss.github.io/pages/office/office.html"
+        url_gh="https://github.com/hactuss/hactuss.github.io"
+      />
+      <ProjectCard
+        title="It is spooky month?"
+        buildtime="Sep 15. 2025"
+        discription="a small jokewebsite I built in reference to the Spooky Month indie animation series. Shows something special in october."
+        url="https://isitspookymonth.vercel.app"
+        url_gh="https://github.com/hactuss/isitspookymonthdotcom"
+      />
+      <ProjectCard
+        title="Ultrakill Terminal"
+        buildtime="Nov 29. 2025"
+        discription="a recreation of the terminals from the indiegame 'Ultrakill'"
+        url="https://uk-terminal.vercel.app"
+        url_gh="https://github.com/hactuss/uk-terminal"
+      />
+      <ProjectCard
+        title="Aero Calendar"
+        buildtime="Dec 9. 2025"
+        discription="A simple yet funcional calendar in the Aero style"
+        url="https://aerocalendar.vercel.app"
+        url_gh="https://github.com/hactuss/aerocalendar"
+      />
+    </div>
+    <hr />
+    <div>
+      <h2>Personality</h2>
+      <div>
+        <h3>Hobbies</h3>
+        <ul>
+          <li>Videoediting</li>
+          <li>
+            Experimenting with Linux (Specifically <a
+              id="marked4"
+              href="https://nixos.org/">NixOS</a
+            >)
+          </li>
+          <li>Drawing comics</li>
+          <li>Reading books regarding my hobbies or general literature</li>
+          <li>Gaming</li>
+        </ul>
+      </div>
+    </div>
+    <hr />
+    <footer>
+      <p>
+        Contact me via Email: <a href="mailto:hactuss@proton.me"
+          ><span id="marked1">hactuss@proton.me</span></a
+        >
+      </p>
+    </footer>
+  </main>
 </main>
 
 <style>
-	* {
-		cursor: url('../lib/assets/images/Cursor.png'), auto;
-	}
-	#SIDEBAR {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		background-color: rgb(50, 50, 50);
-	}
-	#SIDEBAR_TEXT {
-		writing-mode: vertical-lr;
-		margin-top: 1rem;
-		margin-bottom: 1rem;
-	}
-	#MAIN_CONTAINER {
-		margin-left: 12.6%;
-		margin-right: 12.6%;
-		display: flex;
-		flex-direction: row;
-		height: 100%;
-		backdrop-filter: brightness(0);
+  * {
+    cursor: url("../lib/assets/images/Cursor.png"), auto;
+  }
+  #SIDEBAR {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: rgb(50, 50, 50);
+  }
+  #SIDEBAR_TEXT {
+    writing-mode: vertical-lr;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+  }
+  #MAIN_CONTAINER {
+    margin-left: 12.6%;
+    margin-right: 12.6%;
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    backdrop-filter: brightness(0);
 
-		filter: brightness(1);
-	}
-	#MAIN_CONTENTS {
-		display: flex;
-		flex-direction: column;
-	}
-	/*
+    filter: brightness(1);
+  }
+  #MAIN_CONTENTS {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .header-1 {
+    width: 100%;
+  }
+  /*
     #SIDEBAR_LOGO {
         background-color: rgb(50 50 50);
         width: fit-content;
@@ -161,44 +179,44 @@
     #Projects_wrapper {
         border: solid 1px white;
     }*/
-	img {
-		width: 2.25rem;
-	}
-	span {
-		margin: 0;
-		padding: 0;
-	}
+  img {
+    width: 2.25rem;
+  }
+  span {
+    margin: 0;
+    padding: 0;
+  }
 
-	#marked1 {
-		color: rgb(200 250 20);
-	}
-	#marked2 {
-		color: rgb(234 80 39);
-	}
-	#marked3 {
-		color: rgb(193 106 152);
-	}
-	#marked4 {
-		color: rgb(82 107 171);
-	}
-	#marked5 {
-		color: rgb(140 87 53);
-	}
+  #marked1 {
+    color: rgb(200 250 20);
+  }
+  #marked2 {
+    color: rgb(234 80 39);
+  }
+  #marked3 {
+    color: rgb(193 106 152);
+  }
+  #marked4 {
+    color: rgb(82 107 171);
+  }
+  #marked5 {
+    color: rgb(140 87 53);
+  }
 
-	@media screen and (width <= 500px) {
-		#MAIN_CONTAINER {
-			flex-direction: column;
-			margin: 0;
-		}
-		#SIDEBAR {
-			display: flex;
-			flex-direction: row;
-			align-items: flex-end;
-			justify-content: space-between;
-		}
-		#SIDEBAR_TEXT {
-			writing-mode: horizontal-tb;
-			margin-left: 1rem;
-		}
-	}
+  @media screen and (width <= 500px) {
+    #MAIN_CONTAINER {
+      flex-direction: column;
+      margin: 0;
+    }
+    #SIDEBAR {
+      display: flex;
+      flex-direction: row;
+      align-items: flex-end;
+      justify-content: space-between;
+    }
+    #SIDEBAR_TEXT {
+      writing-mode: horizontal-tb;
+      margin-left: 1rem;
+    }
+  }
 </style>
